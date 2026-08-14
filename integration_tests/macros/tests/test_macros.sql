0bc79b23cea314cca1c815660a884c1270295fb9
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2026-08-14 17:59:59 +0900

    Register live metadata and schema diff tests

--- integration_tests/macros/tests/test_macros.sql
+++ integration_tests/macros/tests/test_macros.sql
@@ -5,6 +5,11 @@
   {% do test_quote_safe_and_mixed_case_identifiers() %}
   {% do test_quote_literal_doubles_single_quotes() %}
   {% do test_external_query_renders_without_planning() %}
+  {% do test_postgres_metadata_remote_sql() %}
+  {% do test_alloydb_metadata_uses_postgres_profile() %}
+  {% do test_metadata_query_wraps_external_query() %}
+  {% do test_schema_diff_detects_add_remove_change() %}
+  {% do test_schema_diff_no_changes() %}
   {% do test_plan_all_native_passthrough() %}
   {% do test_plan_uuid_jsonb_safe_projection() %}
   {% do test_plan_uuid_strict_errors() %}
